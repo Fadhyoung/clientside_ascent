b2490e8c24173efb2830186b5af0b56cda6f4e56
fadhli <fadhlinurhimawan@gmail.com>
2022-12-14 10:46:48 +0700

    first commit

---
+++ src/main/java/mii/co/id/clientside_ascent/ClientsideAscentApplication.java
@@ -0,0 +1,13 @@
+package mii.co.id.clientside_ascent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsideAscentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsideAscentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mii/co/id/clientside_ascent/ClientsideAscentApplicationTests.java
@@ -0,0 +1,13 @@
+package mii.co.id.clientside_ascent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsideAscentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
